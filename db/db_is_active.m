6211a2f43efa28585e1019745bb6c7a3e60d6046
tim-neuro <eilers.tim1412@gmail.com>
2025-12-01 16:04:57 -0500

    Delete db directory

--- db/db_is_active.m
+++
@@ -1,9 +0,0 @@
-function enableDatabase = db_is_active
-    enableDatabase = false;
-    if exist('db_load_settings','file')
-        credentials = db_credentials;
-        if ~strcmp(credentials.username,'user')
-            enableDatabase = true;
-        end
-    end
-end
